2bfd7427539f956c45fe84611aab52c0ce98b4a6
针针 <hyejeong@live.cn>
2019-05-19 14:09:56 +0800

    重置样式css

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import './assets/css/reset.css'; // 重置样式
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App)
+}).$mount('#app');
