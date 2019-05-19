cbdbd7185b9c51c609e8f0440c1a49d30b614095
针针 <hyejeong@live.cn>
2019-05-19 14:13:29 +0800

    调整首页路由

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './assets/css/reset.css'; // 重置样式
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
     router,
